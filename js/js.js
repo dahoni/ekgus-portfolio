c08f909e452f36a655e449f65751f893bda354a2
dahoni <49727874+dahoni@users.noreply.github.com>
2019-05-01 09:35:18 +0900

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+   
+    $('.top_txt1,.top_txt2').hide();
+    $('.top_txt1').fadeIn(2000);
+    $('.top_txt2').fadeIn(3000);
+    
+        
+   
+        
+        
+   
+    
+  
+    
+
+
+    
+});
